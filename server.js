beb0cd39637e764b075266192981500f3b2758cb
Santhosh Kumar <varunprabhakar@b-one.com>
2017-12-29 19:03:00 +0530

    Server.js changes

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const path = require('path');
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+//   });
 // Heroku port
 app.listen(process.env.PORT || 8090);
